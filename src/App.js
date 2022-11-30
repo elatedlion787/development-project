4df8cd7d80a45305880ac555009585202cf92591
Mit Ramesh <mithun_ramesh+cs1300@brown.edu>
2022-11-29 23:14:06 -0500

    Pushed new code to work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import ThaiItem from "./components/ThaiItem";
-import SideNavBar from "./components/SideNavBar";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,23 +9,22 @@ import { NavItem } from 'react-bootstrap';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
 /*
 Create our list of data to display on the website using an array of objects
 */
 const ThaiFood = [
-  {name: "Pad Thai", price: 10.99, type: "Noodle", v_friendly: "Yes", image: 'images/download.jpg'},
-  {name: "Tom Yum", price: 15.99, type: "Soup", v_friendly: "No", image: 'images/download-1.jpg'},
-  {name: "Duck Tamarind Curry", price: 30.00, type: "Curry",v_friendly: "No",image: 'images/download-2.jpg'},
-  {name: "Chicken Garlic Noodle Soup", type: "Soup", price: 15.79, v_friendly: "No",image: 'images/download-3.jpg'},
-  {name: "Red Curry", price: 13.99, type: "Curry", v_friendly: "Yes", image: 'images/download-4.jpg'},
-  {name: "Green Curry", price: 20.50, type: "Curry", v_friendly: "Yes", image: 'images/download-5.jpg'},
-  {name: "Panang Curry", price: 15.00, type: "Curry", v_friendly: "No", image: 'images/download-6.jpg'},
-  {name: "Massaman Curry", price: 20.99, type: "Curry",v_friendly: "No", image: 'images/download-14.jpg'},
-  {name: "Pineapple Fried Rice", price: 16.79, type: "Noodle",v_friendly: "Yes",image: 'images/download-7.jpg'},
-  {name: "Pad See Ew", price: 17.49, type: "Noodle", v_friendly: "Yes",image: 'images/download-8.jpg'},
-  {name: "Pad Kee Mao", price: 19.00, type: "Noodle", v_friendly: "Yes", image: 'images/download-9.jpg'},
-  {name: "Crispy Chicken Pad Woon Sen", type: "Noodle", price: 12.00, v_friendly: "No", image: 'images/download-10.jpg'},
+  {name: "Pad Thai", price: 10.99, type: "Noodle", v_friendly: true, image: 'images/download.jpg'},
+  {name: "Tom Yum", price: 15.99, type: "Soup", v_friendly: false, image: 'images/download-1.jpg'},
+  {name: "Duck Tamarind Curry", price: 30.00, type: "Curry",v_friendly: false,image: 'images/download-2.jpg'},
+  {name: "Chicken Garlic Noodle Soup", type: "Soup", price: 15.79, v_friendly: false,image: 'images/download-3.jpg'},
+  {name: "Red Curry", price: 13.99, type: "Curry", v_friendly: true, image: 'images/download-4.jpg'},
+  {name: "Green Curry", price: 20.50, type: "Curry", v_friendly: true, image: 'images/download-5.jpg'},
+  {name: "Panang Curry", price: 15.00, type: "Curry", v_friendly: false, image: 'images/download-6.jpg'},
+  {name: "Massaman Curry", price: 20.99, type: "Curry",v_friendly: false, image: 'images/download-14.jpg'},
+  {name: "Pineapple Fried Rice", price: 16.79, type: "Noodle",v_friendly: true,image: 'images/download-7.jpg'},
+  {name: "Pad See Ew", price: 17.49, type: "Noodle", v_friendly: true,image: 'images/download-8.jpg'},
+  {name: "Pad Kee Mao", price: 19.00, type: "Noodle", v_friendly: true, image: 'images/download-9.jpg'},
+  {name: "Crispy Chicken Pad Woon Sen", type: "Noodle", price: 12.00, v_friendly: false, image: 'images/download-10.jpg'},
 ]
 
 
@@ -34,19 +32,37 @@ const ThaiFood = [
 
 
 function App() {
+  function defaultSort(a, b){
+    return a;
+  }
   // define a cart with length of our ThaiFood array
   const [cart, setCart] = useState(Array(ThaiFood.length).fill(0));
   // aggregator to keep track of all the items in the cart. 
   const [cartTotal, setCartTotal] = useState(0);
-  const [type, setType] = useState("All")
+  const [type, setType] = useState("all")
+  const [data, setData] = useState(ThaiFood);
+  const [diet, setDiet] = useState("all")
+  const [sort, setSort] = useState(null);
 
-  const selectFilterType = eventKey => {
-    console.log("Selected")
-    setType(eventKey);
+  
+  const sorting = (e) => {
+    let sortingMethod = e.target.value;
+    console.log("sortingMethod", sortingMethod)
+    setSort(sortingMethod)
+    sort_func()
   }
 
-  function select (){
-    console.log("selected")
+  function sort_func(){
+    let sort_res
+    if (sort === "price-lh"){
+      console.log("low-hi")
+      sort_res = data.sort(function(a, b){return a.price - b.price})
+      setData(sort_res)
+    } else{
+      console.log("hi-lo")
+      sort_res = data.sort(function(a, b){return b.price - a.price})
+      setData(sort_res)
+    }
   }
 
 
@@ -56,7 +72,15 @@ function App() {
       const tot_price = cartTotal + price
       console.log(tot_price)
       setCartTotal(tot_price);
-      
+  }
+
+  function decrementCart(index, price){
+    if(cart[index] > 0){
+      cart[index] = cart[index] - 1
+      setCart({...cart})
+      const tot_price = cartTotal - price 
+      setCartTotal(tot_price)
+    }
   }
 
   function clearCart(){
@@ -64,60 +88,102 @@ function App() {
       setCartTotal(0);
   }
 
+  const selectFilterType = (e) => {
+    let newFilter = e.target.value;
+    console.log("filter:", newFilter)
+    setType(newFilter)
+  }
+
+  const selectDietFilterType = (e) => {
+    let newDietFilter = e.target.value;
+    console.log("diet_filter", newDietFilter)
+    setDiet(newDietFilter)
+  }
+
+  const matchDietFilterType = item => {
+    if(diet === "all"){
+      return true
+    } else if (diet === "v_friendly"){
+        console.log("Not match")
+        return item.v_friendly
+    } else if (diet === "non_v_friendly"){
+        return !(item.v_friendly)
+    }
+  }
+
+  const matchTypeFilter = item => {
+    if(type == "all"){
+      return true
+    } else{
+      return type === item.type
+    }
+  }
+
   
 
 
   return (
-    
     <div className="App">
-      <Nav onSelect={onSelect} className="container-nav">
+      <div className="header">
+      <h1> ElatedLion787's Thai Resturant</h1>
+      </div>
+      <div>
+      <Nav>
         <Container>
-            <Row>
-                <h5> Vegetarian Friendly </h5>
+          <h4> Filter for Your Desired Food!</h4>
+          <Row>
+          <h5> Choose Dietary Restriction: </h5>
                 <Col sm={4}>
-                    <Nav.Item> <Nav.Link eventKey="all"> </Nav.Link><input type='radio' name='v_friendly' id="all_v_friendly" value='all'/> 
-                    <label for="all_v_friendly"> All Foods</label>
-                    </Nav.Item>
-                    <Nav.Item> <Nav.Link eventKey="v_friendly"> </Nav.Link><input type='radio' name='v_friendly' id="is_v_friendly" value='True'/>
-                    <label for="is_v_friendly"> Vegetarian Friendly</label></Nav.Item>
-                    <Nav.Item><Nav.Link eventKey="not_v_friendly"> </Nav.Link><input type='radio' name='v_friendly' id="is_not_v_friendly" value='False'/>
-                    <label for="is_v_friendly"> Non-Vegetarian Friendly</label>
-                    </Nav.Item> 
+                  <form>
+                    <select name="selectList" onChange={selectDietFilterType}> 
+                      <option value="all"> None </option>
+                      <option value="v_friendly"> Vegetarian Friendly </option>
+                      <option value="non_v_friendly"> Non-Vegetarian Friendly</option>
+                    </select>
+                  </form>
                 </Col>
-            </Row>
+          </Row>
+          <Row>
+          <h5> Choose Type of Food: </h5>
+                <Col sm={4}>
+                  <form>
+                    <select name="selectList" onChange={selectFilterType}> 
+                      <option value="all"> All </option>
+                      <option value="Curry"> Curry </option>
+                      <option value="Noodle"> Noodle</option>
+                      <option value="Soup"> Soup</option>
+                    </select>
+                  </form>
+                </Col>
+          </Row>
+          <Row>
+            <h5> Choose Property to Sort By: </h5>
+            <Col sm={4}>
+                <form>
+                    <Nav.Item> <input type='radio' name='sort' id="price" value='price-lh' onChange={sorting} />
+                    <label for="sort"> Price High-Low</label> </Nav.Item>
 
-            <Row>
-                <h5> Type of Food </h5>
-                <Col sm={4}>
-                    <Nav.Item> <input type='radio' name='type' id="all" value='all'/>
-                    <label for="type"> All </label> </Nav.Item>
-                    <Nav.Item> <input type='radio' name='type' id="Noodle" value='Noodle'/>
-                    <label for="type"> Noodle </label> </Nav.Item>
-                    <Nav.Item> <input type='radio' name='type' id="Soup" value='Soup'/>
-                    <label for="type"> Soup</label> </Nav.Item>
-                    <Nav.Item> <input type='radio' name='type' id="Curry" value='Curry'/>
-                    <label for="type"> Curry</label> </Nav.Item>
-                </Col>
-            </Row>
-            <Row>
-                <h5> Sort By: </h5>
-                <Col sm={4}>
-                    <Nav.Item> <input type='radio' name='sort' id="price" value='price'/>
-                    <label for="type"> Price Low-High</label> </Nav.Item>
-
-                    <Nav.Item> <input type='radio' name='sort' id="price" value='price'/>
-                    <label for="type"> Price High-Low</label> </Nav.Item>
-                </Col>
-            </Row>
+                    <Nav.Item> <input type='radio' name='sort' id="price" value='price-hl' onChange={sorting}/>
+                    <label for="sort"> Price Low-High</label> </Nav.Item>
+                </form>
+            </Col>
+          </Row>
+          <Row>
+          <h3> Total Price of Items in Cart: ${(Math.round(cartTotal * 100) / 100).toFixed(2)} </h3>
+          </Row>
+          
         </Container>
         </Nav>
-      <h1> ElatedLion787's Thai Resturant</h1>
+      
+
       <div className="wrapper">
-        {ThaiFood.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-          <ThaiItem item={item} index={index} onClick={incrementCart}/>
+        {data.map((item, index) => ( // TODO: map bakeryData to   
+          matchDietFilterType(item) &&
+          matchTypeFilter(item) &&
+          <ThaiItem item={item} index={index} onClickAdd={incrementCart} onClickRemove={decrementCart} cart={cart}/>
         ))}
-        <h3> Total Price: {cartTotal} </h3>
       </div>
+      </div> 
     </div>
   );
 }
--- src/components/ThaiItem.js
+++ src/components/ThaiItem.js
@@ -1,14 +1,21 @@
 // TODO: create a component that displays a single bakery item
-export default function ThaiItem({item, index, onClick}){
+function v_friendly_text(item_text){
+    if (item_text === true){
+        return "Yes"
+    } return "No"
+}
+
+export default function ThaiItem({item, index, onClickAdd, onClickRemove, cart}){
     return (
         <div className="bakery-item">
         <img src={item.image} alt={item.name} />
         <h3>{item.name}</h3>
         <p>Price: ${item.price}</p>
         <p> Type: {item.type} </p>
-        <p> Vegetarian Friendly?: {item.v_friendly}</p>
-        <p>${item.price}</p>
-        <button onClick={() => onClick(index, item.price)}> Add to cart</button>
+        <p> Vegetarian Friendly: {v_friendly_text(item.v_friendly)}</p>
+        <p> Quantity: {cart[index]} </p> 
+        <button onClick={() => onClickAdd(index, item.price)}> Add to cart</button>
+        <button onClick={() => onClickRemove(index, item.price)}> Remove from Cart</button>
         </div>
     );
 }
